93dc34193ee5ccadbd747d9dbaf03c2e95ab244d
chenyuhao <cccchenyuhao@gmail.com>
2023-12-04 01:39:29 +0800

    pagechange

--- public/tracker.js
+++ public/tracker.js
@@ -2,7 +2,16 @@
   let initialized;
 
   const endpoint = "http://localhost:4002/api";
+  const hook = (_this, method, before, after) => {
+    const orig = _this[method];
 
+    return (...args) => {
+      before?.apply(null, args);
+      const res = orig.apply(_this, args);
+      after?.apply(null, args);
+      return res;
+    };
+  };
   function send(payload = {}, type = "event") {
     const headers = {
       "Content-Type": "application/json",
@@ -23,68 +32,71 @@
     history,
   } = window;
 
-  // 进入页面时触发
-  document.addEventListener("readystatechange", () => {
-    if (document.readyState === "complete" && !initialized) {
-      init();
-      initialized = true;
-    }
-  });
-
-  // 页面切换,关闭时触发
-  document.addEventListener("visibilitychange", function logData() {
-    if (document.visibilityState === "hidden") {
-      console.log("hidden");
-      navigator.sendBeacon(endpoint);
-    }
-  });
-
-  // 命令式路由切换时触发
-  history.pushState = hook(history, "pushState", handlePush);
-  history.replaceState = hook(history, "replaceState", handlePush);
-
-  function handlePush() {
-    send(getPayload());
-  }
-
-  // 鼠标点击浏览器前进后退时触发
-  window.addEventListener("popstate", async () => {
-    send(getPayload());
-  });
-
   const { hostname, pathname, search } = location;
 
   let title = document.title;
 
   let currentUrl = `${pathname}${search}`;
   let currentreferrer = document.referrer;
+  const { website } = document.currentScript.dataset;
+  let currentPathname;
+
   function getPayload() {
     return {
       website,
-      hostname,
+      hostname: location.hostname,
       screen: { width, height },
       language,
       title,
-      url: currentUrl,
-      referrer: currentreferrer,
+      url: location.pathname,
+      referrer: document.referrer,
     };
   }
 
-  const { website } = document.currentScript.dataset;
+  // 进入页面时触发 "enter"
+  document.addEventListener("readystatechange", () => {
+    if (document.readyState === "complete") {
+      console.log("readystatechange complete", location);
+    }
+  });
 
-  const hook = (_this, method, callback) => {
-    const orig = _this[method];
+  // 页面切换,关闭时触发 "leave"
+  document.addEventListener("visibilitychange", function logData() {
+    if (document.visibilityState === "hidden") {
+      console.log("leave visibilitychange", location);
+      // navigator.sendBeacon(endpoint);
+    }
 
-    return (...args) => {
-      callback.apply(null, args);
+    if (document.visibilityState === "visible") {
+      console.log("enter visibilitychange", getPayload());
+    }
+  });
 
-      return orig.apply(_this, args);
-    };
-  };
+  // 命令式路由切换时触发 "leave"
+  history.pushState = hook(history, "pushState", handleBefore, handleAfter);
+  history.replaceState = hook(
+    history,
+    "replaceState",
+    handleBefore,
+    handleAfter
+  );
 
-  async function init() {
-    await send(getPayload());
-    // await send(getPayload());
-    initialized = true;
+  function handleBefore() {
+    // send(getPayload());
+    console.log("leave,pushState be", location.pathname);
   }
+
+  function handleAfter() {
+    // send(getPayload());
+    currentPathname = location.pathname;
+    console.log("leave,pushState af", location.pathname);
+  }
+
+  // 鼠标点击浏览器前进后退时触发
+  window.addEventListener("popstate", async (e) => {
+    // send(getPayload());
+    console.log(currentPathname);
+    currentPathname = location.pathname;
+    console.log("popstate", location.pathname);
+  });
 })(window);
